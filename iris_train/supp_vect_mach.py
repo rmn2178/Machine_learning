a139b32ee893a9b408d35a98edceda3d4eb4bdf9
rmn2178 <gokulrmn2178@gmail.com>
2025-11-17 22:27:38 +0530

    feat : new SVM code was added

--- iris_train/supp_vect_mach.py
+++ iris_train/supp_vect_mach.py
@@ -1,6 +1,11 @@
 # This is a code of loading iris flower dataset
+# SUPPORT VECTOR MATH
 import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.svm import SVC
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+import pickle
 
 # Loading the data
 iris = load_iris()
@@ -31,3 +36,33 @@ df['target'] = iris.target
 # We are creating a new column named flower_name
 df['flower_names'] = df.target.apply(lambda x: iris.target_names[x])
 
+# Separates the data
+df1 = df[df.target==0]
+df2 = df[df.target==1]
+df3 = df[df.target==2]
+
+plt.scatter(df1['petal length (cm)'], df1['petal width (cm)'],color='red', marker = '*')
+plt.scatter(df2['petal length (cm)'], df2['petal width (cm)'],color='green', marker = '*')
+plt.scatter(df3['petal length (cm)'], df3['petal width (cm)'],color='blue', marker = '*')
+plt.xlabel('petal length (cm)')
+plt.ylabel('petal width (cm)')
+
+plt.show()
+
+# Splitting the datasets
+X = df.drop(['target','flower_names'], axis='columns')
+Y = df.target
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+# Here the main model
+model = SVC()
+model.fit(X_train, Y_train)
+
+# This code saves the model
+with open('svm_model.pkl', 'wb') as file:
+    pickle.dump(model, file)
+
+with open('svm_model.pkl', 'rb') as file:
+    model_x = pickle.load(file)
+
+print(model_x.score(X_test, Y_test))
